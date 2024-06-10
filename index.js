c7e0bb315efa7123d510a96f7a991e86308b03bd
Manoj Singh Negi <www.msnegi@gmail.com>
2024-06-10 16:37:23 +0530

    api fixes

--- index.js
+++ index.js
@@ -33,36 +33,3 @@ app.use((err, req, res, next) => {
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
 
-
-// const express = require("express");
-// const cors = require('cors');
-// const { createSequelizeInstance } = require('./connection');
-// const router = require('./routes/routes');
-// const app = express();
-// const PORT = 8000;
-
-// const sequelize = createSequelizeInstance();
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connected to the database');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-// app.use(cors({
-//   origin: '*', 
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Access-Token'] 
-// }));
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-// app.use("/", router);
-
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).send('Internal Server Error');
-// });
-
-// app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
