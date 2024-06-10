df7e12441af9ab6e8613cc12fc5ee0fb277f7e81
Manoj Singh Negi <www.msnegi@gmail.com>
2024-06-10 17:57:31 +0530

    file uploading error fixes 2

--- helpers/s3bucket.js
+++ helpers/s3bucket.js
@@ -5,29 +5,108 @@ require('dotenv').config();
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION // Ensure region is set
 });
 
 const s3 = new AWS.S3();
 const bucketName = process.env.AWS_BUCKET_NAME;
 
 async function uploadImage(filePath, originalFilename, folder) {
-    try {
-        const timestamp = new Date().getTime(); 
-        const fileStream = fs.createReadStream(filePath);
-        const sanitizedFilename = originalFilename.replace(/[^\w\s.-]/g, '_');
-        const uploadParams = {
-            Bucket: bucketName,
-            Key: `${folder}/${timestamp}_${sanitizedFilename}`, 
-            Body: fileStream
-        };
+    const timestamp = new Date().getTime(); 
+    const sanitizedFilename = originalFilename.replace(/[^\w\s.-]/g, '_');
+    const s3Key = `${folder}/${timestamp}_${sanitizedFilename}`;
 
-        const uploadResult = await s3.upload(uploadParams).promise();
-        return uploadResult.Key; 
+    const fileStream = fs.createReadStream(filePath);
+    const fileStats = fs.statSync(filePath);
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Key: s3Key,
+        Body: fileStream
+    };
+
+    try {
+        if (fileStats.size > 5 * 1024 * 1024) { // If file is larger than 5MB, use multipart upload
+            const multipartUpload = s3.createMultipartUpload({
+                Bucket: bucketName,
+                Key: s3Key,
+            }).promise();
+
+            const parts = [];
+            const partSize = 5 * 1024 * 1024; // Each part is 5MB
+            let partNumber = 1;
+
+            for (let offset = 0; offset < fileStats.size; offset += partSize) {
+                const partParams = {
+                    Bucket: bucketName,
+                    Key: s3Key,
+                    PartNumber: partNumber,
+                    UploadId: multipartUpload.UploadId,
+                    Body: fileStream,
+                };
+
+                const part = await s3.uploadPart(partParams).promise();
+                parts.push({ ETag: part.ETag, PartNumber: partNumber });
+
+                partNumber++;
+            }
+
+            const completeMultipartUploadParams = {
+                Bucket: bucketName,
+                Key: s3Key,
+                UploadId: multipartUpload.UploadId,
+                MultipartUpload: {
+                    Parts: parts,
+                },
+            };
+
+            await s3.completeMultipartUpload(completeMultipartUploadParams).promise();
+        } else {
+            await s3.upload(uploadParams).promise();
+        }
+
+        return s3Key;
     } catch (err) {
         console.error("Error uploading file:", err);
         throw err;
+    } finally {
+        fileStream.destroy(); // Ensure the stream is closed properly
     }
 }
 
 module.exports = uploadImage;
+
+// const AWS = require('aws-sdk');
+// const fs = require('fs');
+// const path = require('path');
+// require('dotenv').config(); 
+
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET_KEY
+// });
+
+// const s3 = new AWS.S3();
+// const bucketName = process.env.AWS_BUCKET_NAME;
+
+// async function uploadImage(filePath, originalFilename, folder) {
+//     try {
+//         const timestamp = new Date().getTime(); 
+//         const fileStream = fs.createReadStream(filePath);
+//         const sanitizedFilename = originalFilename.replace(/[^\w\s.-]/g, '_');
+//         const uploadParams = {
+//             Bucket: bucketName,
+//             Key: `${folder}/${timestamp}_${sanitizedFilename}`, 
+//             Body: fileStream
+//         };
+
+//         const uploadResult = await s3.upload(uploadParams).promise();
+//         return uploadResult.Key; 
+//     } catch (err) {
+//         console.error("Error uploading file:", err);
+//         throw err;
+//     }
+// }
+
+// module.exports = uploadImage;
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const cors = require('cors');
 const { createSequelizeInstance } = require('./connection');
 const router = require('./routes/routes');
-const fileUpload = require('express-fileupload'); // Import express-fileupload
-require('dotenv').config(); // Ensure environment variables are loaded
-
 const app = express();
 const PORT = 8000;
 
@@ -24,57 +21,15 @@ app.use(cors({
   credentials: true
 }));
 
-// Increase payload size limits
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
-app.use(express.json({ limit: '50mb' }));
-
-// Use express-fileupload with increased file size limit
-app.use(fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
-}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/", router);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send('Internal Server Error');
 });
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
-
-// const express = require("express");
-// const cors = require('cors');
-// const { createSequelizeInstance } = require('./connection');
-// const router = require('./routes/routes');
-// const app = express();
-// const PORT = 8000;
-
-// const sequelize = createSequelizeInstance();
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connected to the database');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-// app.use(cors({
-//   origin: '*',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Access-Token'],
-//   credentials: true
-// }));
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// app.use("/", router);
-
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).send('Internal Server Error');
-// });
-
-// app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
 
