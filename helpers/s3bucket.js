f80d0e0affa94012d15f7f67d972fc94fa260436
Manoj Singh Negi <www.msnegi@gmail.com>
2024-09-05 16:46:09 +0530

    aws sdk warning fixes

--- helpers/s3bucket.js
+++ helpers/s3bucket.js
@@ -1,14 +1,51 @@
-const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk');
+// const fs = require('fs');
+// const path = require('path');
+// require('dotenv').config(); 
+
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET_KEY
+// });
+
+// const s3 = new AWS.S3();
+// const bucketName = process.env.AWS_BUCKET_NAME;
+
+// async function uploadImage(filePath, originalFilename, folder) {
+//     try {
+//         const timestamp = new Date().getTime(); 
+//         const fileStream = fs.createReadStream(filePath);
+//         const sanitizedFilename = originalFilename.replace(/[^\w\s.-]/g, '_');
+//         const uploadParams = {
+//             Bucket: bucketName,
+//             Key: `${folder}/${timestamp}_${sanitizedFilename}`, 
+//             Body: fileStream
+//         };
+
+//         const uploadResult = await s3.upload(uploadParams).promise();
+//         return uploadResult.Key; 
+//     } catch (err) {
+//         console.error("Error uploading file:", err);
+//         throw err;
+//     }
+// }
+
+// module.exports = uploadImage;
+
+
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config(); 
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
 });
 
-const s3 = new AWS.S3();
 const bucketName = process.env.AWS_BUCKET_NAME;
 
 async function uploadImage(filePath, originalFilename, folder) {
@@ -22,8 +59,9 @@ async function uploadImage(filePath, originalFilename, folder) {
             Body: fileStream
         };
 
-        const uploadResult = await s3.upload(uploadParams).promise();
-        return uploadResult.Key; 
+        const command = new PutObjectCommand(uploadParams);
+        const uploadResult = await s3.send(command);
+        return uploadParams.Key; 
     } catch (err) {
         console.error("Error uploading file:", err);
         throw err;
